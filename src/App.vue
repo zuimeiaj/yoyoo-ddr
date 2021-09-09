519e5e88f781695608b37610e63b881cd34fa66b
Jun Yao <2498683974@qq.com>
2021-09-09 08:48:49 +0800

    修改demo友情链接

--- src/App.vue
+++ src/App.vue
@@ -31,8 +31,8 @@
         </DDR>
       </div>
       <footer class="footer">
-        <a target="_blank" href="https://app.yoyoo.ink">YOYOO原型设计 - 一个在线高保真原型设计工具</a>
-        <a target="_blank" href="https://www.fechat.ink">FE社区 - 一个基于JavaScript的垂直化前端技术社区</a>
+        <a target="_blank" href="https://zuimeiaj.github.io/yoyoo/">YOYOO原型设计 - 一个在线高保真原型设计工具</a>
+        <a target="_blank" href="https://vivw.org/">Vivw社区 - 一个基于JavaScript的垂直化前端技术社区</a>
         <a target="_blank" href="https://github.com/zuimeiaj/yoyoo-ddr">Github</a>
       </footer>
     </div>
