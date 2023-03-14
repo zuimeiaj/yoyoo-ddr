ca53e1df1aa78e9eaf29a606d03068bc03234414
Jun Yao <zuimeiaj@outlook.com>
2023-03-14 10:52:47 +0800

    fix the selection  of the demoproject

--- src/examples/vseditor/plugins/plugin-selection.vue
+++ src/examples/vseditor/plugins/plugin-selection.vue
@@ -1,10 +1,10 @@
 <script>
-import { Interaction } from '@/examples/utils/dom'
-import DDrComponent from '@/components/ddr'
-import { EVENT_COMPONENT_UNSELECT } from '../event-enums'
-import { getComponentRef, getComponentRefsById } from '@/examples/utils/ref'
-import { getBoundingRect } from '@/components/ddr/helper'
-import { registerSelectionActions } from './plugin-selection-actions'
+import { Interaction } from '@/examples/utils/dom';
+import DDrComponent from '@/components/ddr';
+import { EVENT_COMPONENT_UNSELECT } from '../event-enums';
+import { getComponentRef, getComponentRefsById } from '@/examples/utils/ref';
+import { getBoundingRect } from '@/components/ddr/helper';
+import { registerSelectionActions } from './plugin-selection-actions';
 /**
  * @description
  *  1、为编辑器增加批量选择功能
@@ -26,169 +26,167 @@ export default {
       selectionActive: false,
       selectionHandler: ['tl', 'tr', 'br', 'bl'],
       selectionArea: { x: 0, y: 0, width: 0, height: 0 },
-    }
+    };
   },
   watch: {
     selectionActive(value) {
       if (!value) {
-        this.selectedComponents = []
-        this.selectionTransform = { x: 0, y: 0, width: 0, rotation: 0 }
-        this.componentRefs = []
+        this.selectedComponents = [];
+        this.selectionTransform = { x: 0, y: 0, width: 0, rotation: 0 };
+        this.componentRefs = [];
       }
     },
   },
   methods: {
     getContains() {
-      const rect = this.selectionArea
-      const controls = this.application.controls
+      const rect = this.selectionArea;
+      const controls = this.application.controls;
       // 被包含的元素才会被选中
       const crossComponents = controls.filter((item) => {
-        let transform = getBoundingRect(item.transform)
-        let isXContain = transform.left > rect.x && transform.right < rect.x + rect.width
-        let isYContain = transform.top > rect.y && transform.bottom < rect.y + rect.height
-        return isXContain && isYContain
-      })
+        let transform = getBoundingRect(item.transform, 1);
+        let isXContain = transform.left > rect.x && transform.right < rect.x + rect.width;
+        let isYContain = transform.top > rect.y && transform.bottom < rect.y + rect.height;
+        return isXContain && isYContain;
+      });
       this.selectedComponents = crossComponents.map((item) => ({
         id: item.id,
         ...item.transform,
-      }))
+      }));
     },
     handleSelectionStart() {
-      this.eventbus.$emit(EVENT_COMPONENT_UNSELECT)
-      this.selectionActive = false
+      this.eventbus.$emit(EVENT_COMPONENT_UNSELECT);
+      this.selectionActive = false;
     },
     handleSelectionMove({ x, y, downClientX, downClientY }) {
       if (x < 0) {
-        x = Math.abs(x)
-        downClientX -= x
+        x = Math.abs(x);
+        downClientX -= x;
       }
       if (y < 0) {
-        y = Math.abs(y)
-        downClientY -= y
+        y = Math.abs(y);
+        downClientY -= y;
       }
-      this.selectionArea = { x: downClientX, y: downClientY, width: x, height: y }
-      this.getContains()
+      this.selectionArea = { x: downClientX, y: downClientY, width: x, height: y };
+      this.getContains();
     },
     handleSelectionEnd() {
       if (this.selectedComponents.length == 1) {
-        this.application.handleSelect(
-          this.application.controls.find((item) => item.id === this.selectedComponents[0].id)
-        )
-        this.selectedComponents = []
+        this.application.handleSelect(this.application.controls.find((item) => item.id === this.selectedComponents[0].id));
+        this.selectedComponents = [];
       } else if (this.selectedComponents.length > 1) {
-        this.application.handleUnselect()
-        this.createSelectionTransform()
+        this.application.handleUnselect();
+        this.createSelectionTransform();
       }
-      this.selectionArea = { x: 0, y: 0, width: 0, height: 0 }
+      this.selectionArea = { x: 0, y: 0, width: 0, height: 0 };
     },
     createSelectionTransform() {
-      let x = []
-      let y = []
-      let x1 = []
-      let y1 = []
-      let ids = []
+      let x = [];
+      let y = [];
+      let x1 = [];
+      let y1 = [];
+      let ids = [];
       this.selectedComponents.forEach((item) => {
-        ids.push(item.id)
-        item = getBoundingRect(item)
-        x.push(item.left)
-        y.push(item.top)
-        x1.push(item.right)
-        y1.push(item.bottom)
-      })
-      let left = Math.min(...x)
-      let top = Math.min(...y)
-      let right = Math.max(...x1)
-      let bottom = Math.max(...y1)
-      this.selectionActive = true
+        ids.push(item.id);
+        item = getBoundingRect(item, 1);
+        x.push(item.left);
+        y.push(item.top);
+        x1.push(item.right);
+        y1.push(item.bottom);
+      });
+      let left = Math.min(...x);
+      let top = Math.min(...y);
+      let right = Math.max(...x1);
+      let bottom = Math.max(...y1);
+      this.selectionActive = true;
       this.selectionTransform = {
         x: left,
         y: top,
         width: right - left,
         height: bottom - top,
         rotation: 0,
-      }
-      this.getSelectedComponentRefs(ids, this.selectionTransform)
+      };
+      this.getSelectedComponentRefs(ids, this.selectionTransform);
     },
     getSelectedComponentRefs(ids, { x, y, width, height }) {
       this.componentRefs = getComponentRefsById(ids).map((item) => {
         // 记录组件在选区的位置和宽度百分比
-        item._xPercent = (item.transform.x - x) / width
-        item._yPercent = (item.transform.y - y) / height
-        item._wPercent = item.transform.width / width
-        item._hPercent = item.transform.height / height
-        return item
-      })
+        item._xPercent = (item.transform.x - x) / width;
+        item._yPercent = (item.transform.y - y) / height;
+        item._wPercent = item.transform.width / width;
+        item._hPercent = item.transform.height / height;
+        return item;
+      });
     },
     getSelectedComponentTransform() {
       this.selectedComponents = this.selectedComponents.map((item) => {
-        let t = getComponentRef(item.id).transform
-        item.x = t.x
-        item.y = t.y
-        item.width = t.width
-        item.height = t.height
-        return item
-      })
+        let t = getComponentRef(item.id).transform;
+        item.x = t.x;
+        item.y = t.y;
+        item.width = t.width;
+        item.height = t.height;
+        return item;
+      });
     },
     handleSelectionDrag(e) {
-      e.stopPropagation()
-      let area = this.$refs.ddr.transform
+      e.stopPropagation();
+      let area = this.$refs.ddr.transform;
       this.componentRefs.forEach((item) => {
-        item.transform.x = Math.round(area.x + area.width * item._xPercent)
-        item.transform.y = Math.round(area.y + area.height * item._yPercent)
-      })
-      this.getSelectedComponentTransform()
+        item.transform.x = Math.round(area.x + area.width * item._xPercent);
+        item.transform.y = Math.round(area.y + area.height * item._yPercent);
+      });
+      this.getSelectedComponentTransform();
     },
     stopPropagation(e) {
-      e.stopPropagation()
+      e.stopPropagation();
     },
     handleSelectionDragEnd(e, transform) {
-      e.stopPropagation()
-      let changes = {}
+      e.stopPropagation();
+      let changes = {};
       this.componentRefs.forEach((item) => {
         changes[item.id] = {
           key: 'transform',
           value: { ...item.transform },
-        }
-      })
-      this.selectionTransform = transform
-      this.application.batchUpdateControlValue(changes)
+        };
+      });
+      this.selectionTransform = transform;
+      this.application.batchUpdateControlValue(changes);
     },
 
     handleSelectionResize(e) {
-      e.stopPropagation()
-      let area = this.$refs.ddr.transform
+      e.stopPropagation();
+      let area = this.$refs.ddr.transform;
       this.componentRefs.forEach((item) => {
-        item.transform.x = Math.round(area.x + area.width * item._xPercent)
-        item.transform.y = Math.round(area.y + area.height * item._yPercent)
-        item.transform.width = Math.round(area.width * item._wPercent)
-        item.transform.height = Math.round(area.height * item._hPercent)
-      })
-      this.getSelectedComponentTransform()
+        item.transform.x = Math.round(area.x + area.width * item._xPercent);
+        item.transform.y = Math.round(area.y + area.height * item._yPercent);
+        item.transform.width = Math.round(area.width * item._wPercent);
+        item.transform.height = Math.round(area.height * item._hPercent);
+      });
+      this.getSelectedComponentTransform();
     },
   },
 
   mounted() {
-    this.componentRefs = []
-    let element = this.application.getEditorView().getWrapperElement()
+    this.componentRefs = [];
+    let element = this.application.getEditorView().getWrapperElement();
 
     Interaction.init(element, {
       onStart: () => {
-        this.handleSelectionStart()
+        this.handleSelectionStart();
       },
       onMove: (interaction) => {
-        this.handleSelectionMove(interaction)
+        this.handleSelectionMove(interaction);
       },
       onEnd: () => {
-        this.handleSelectionEnd()
+        this.handleSelectionEnd();
       },
-    })
+    });
     // register
-    registerSelectionActions(this)
+    registerSelectionActions(this);
   },
   render() {
-    const { x, y, width, height } = this.selectionArea
+    const { x, y, width, height } = this.selectionArea;
     return (
-      <div data-plugin="selection" class="plugin-selection">
+      <div data-plugin='selection' class='plugin-selection'>
         <div
           style={`
             display:${width > 0 ? 'block' : 'none'};
@@ -196,12 +194,12 @@ export default {
             height:${height}px;
             left:${x}px;
             top:${y}px`}
-          class="selection-box"
+          class='selection-box'
         />
         {this.selectedComponents.map((item) => {
           return (
             <div
-              class="selection-selected-component-bordered"
+              class='selection-selected-component-bordered'
               style={`
                 transform:rotate(${item.rotation}deg);
                 left:${item.x}px;
@@ -210,12 +208,12 @@ export default {
                 height:${item.height}px`}
               key={item.id}
             />
-          )
+          );
         })}
 
         <DDrComponent
-          ref="ddr"
-          data-component="true"
+          ref='ddr'
+          data-component='true'
           rotatable={false}
           grid={this.grid}
           resizeHandler={this.selectionHandler}
@@ -229,9 +227,9 @@ export default {
           value={this.selectionTransform}
         />
       </div>
-    )
+    );
   },
-}
+};
 </script>
 
 <style lang="less" scoped>
